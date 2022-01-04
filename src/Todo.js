09366b4ca5af1771a6888152e630cc0793b0d929
Israa Alnazer <israa_alnazer@hotmail.com>
2022-01-04 20:52:12 +0200

    add linethrough functionality to Todo text

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
+import './Todo.css'
 
 class Todo extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this)
+        this.handleTextClick = this.handleTextClick.bind(this)
     }
     handleClick() {
         this.props.deleteTodo(this._reactInternals.key)
     }
+    handleTextClick() {
+        this.props.linethroughTodo(this._reactInternals.key)
+    }
     render() {
+        let className=""
+        if(this.props.linethrough){
+            className = "Todo-text-linethrough"
+        }
         return (
             <div>
-                <div>{this.props.text}
+                <div className={className} onClick={this.handleTextClick}>{this.props.text}
                 </div>
                 <button>Edit Me</button>
                 <button onClick={this.handleClick}>Delete Me</button>
--- src/TodoList.js
+++ src/TodoList.js
@@ -9,12 +9,13 @@ class TodoList extends Component {
         super(props);
         this.handleAddTodo = this.handleAddTodo.bind(this) 
         this.handleDeleteTodo = this.handleDeleteTodo.bind(this)
+        this.handleLinethroughTodo = this.handleLinethroughTodo.bind(this)
         this.state = {
             todos: []
         }
     }
     handleAddTodo(todo) {
-        let newTodo = { ...todo, key: uuidv4() }
+        let newTodo = { ...todo, key: uuidv4(), linethrough: false }
         let oldTodos = this.state.todos
         let newTodos = [...oldTodos, newTodo]
         this.setState({
@@ -28,8 +29,20 @@ class TodoList extends Component {
             todos: newTodos
         }));
     }
+    handleLinethroughTodo(todoKey){  
+        let index = this.state.todos.findIndex(element => element.key == todoKey)
+        let newTodo = {
+            todo: this.state.todos[index].todo,
+            linethrough: !this.state.todos[index].linethrough,
+            key: this.state.todos[index].key
+        }
+        let newTodos = [...this.state.todos.slice(0, index),newTodo, ...this.state.todos.slice(index + 1)]
+        this.setState(st => ({
+            todos: newTodos
+        }));
+    }
     render() {
-        let todos = this.state.todos.map(todoElement => <Todo text={todoElement.todo} key={todoElement.key} deleteTodo={this.handleDeleteTodo}/>)
+        let todos = this.state.todos.map(todoElement => <Todo linethrough = {todoElement.linethrough} text={todoElement.todo} key={todoElement.key} deleteTodo={this.handleDeleteTodo} linethroughTodo={this.handleLinethroughTodo}/>)
         return (
             <div>
                 <NewTodoForm addTodo={this.handleAddTodo} />
