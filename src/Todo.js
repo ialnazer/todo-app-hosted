f166dc3c0a99092706fd29311616ee583dd7a26d
Israa Alnazer <israa_alnazer@hotmail.com>
2022-01-04 21:57:03 +0200

    add edit Todo functionality

--- src/Todo.js
+++ src/Todo.js
@@ -4,26 +4,54 @@ import './Todo.css'
 class Todo extends Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
         this.handleTextClick = this.handleTextClick.bind(this)
+        this.handleEditClick = this.handleEditClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.state = {
+            editedTodo: this.props.text
+        }
     }
-    handleClick() {
+    handleDeleteClick() {
         this.props.deleteTodo(this._reactInternals.key)
+    }
+    handleEditClick() {
+        this.props.toeditTodo(this._reactInternals.key)
     }
     handleTextClick() {
         this.props.linethroughTodo(this._reactInternals.key)
     }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.editTodo(this._reactInternals.key, this.state.editedTodo)
+    }
     render() {
-        let className=""
-        if(this.props.linethrough){
+        let className = ""
+        if (this.props.linethrough) {
             className = "Todo-text-linethrough"
+        }
+        let todo =
+            <div>
+                <div className={className} onClick={this.handleTextClick}>{this.props.text}</div>
+                <button onClick={this.handleEditClick}>Edit Me</button>
+                <button onClick={this.handleDeleteClick}>Delete Me</button>
+            </div>
+        if (this.props.toedit) {
+            todo =
+                <form>
+                    <input name='editedTodo' value={this.state.editedTodo} onChange={this.handleChange}></input>
+                    <button onClick={this.handleSubmit}>Save</button>
+                </form>
         }
         return (
             <div>
-                <div className={className} onClick={this.handleTextClick}>{this.props.text}
-                </div>
-                <button>Edit Me</button>
-                <button onClick={this.handleClick}>Delete Me</button>
+                {todo}
             </div>
         );
     }
--- src/TodoList.js
+++ src/TodoList.js
@@ -10,12 +10,14 @@ class TodoList extends Component {
         this.handleAddTodo = this.handleAddTodo.bind(this) 
         this.handleDeleteTodo = this.handleDeleteTodo.bind(this)
         this.handleLinethroughTodo = this.handleLinethroughTodo.bind(this)
+        this.handleToEditTodo = this.handleToEditTodo.bind(this)
+        this.handleEditTodo = this.handleEditTodo.bind(this)
         this.state = {
             todos: []
         }
     }
     handleAddTodo(todo) {
-        let newTodo = { ...todo, key: uuidv4(), linethrough: false }
+        let newTodo = { ...todo, key: uuidv4(), linethrough: false, toedit: false }
         let oldTodos = this.state.todos
         let newTodos = [...oldTodos, newTodo]
         this.setState({
@@ -33,8 +35,35 @@ class TodoList extends Component {
         let index = this.state.todos.findIndex(element => element.key == todoKey)
         let newTodo = {
             todo: this.state.todos[index].todo,
-            linethrough: !this.state.todos[index].linethrough,
-            key: this.state.todos[index].key
+            key: this.state.todos[index].key,
+            toedit: this.state.todos[index].toedit,
+            linethrough: !this.state.todos[index].linethrough
+        }
+        let newTodos = [...this.state.todos.slice(0, index),newTodo, ...this.state.todos.slice(index + 1)]
+        this.setState(st => ({
+            todos: newTodos
+        }));
+    }
+    handleToEditTodo(todoKey){  
+        let index = this.state.todos.findIndex(element => element.key == todoKey)
+        let newTodo = {
+            todo: this.state.todos[index].todo,
+            linethrough: this.state.todos[index].linethrough,
+            key: this.state.todos[index].key,
+            toedit: true
+        }
+        let newTodos = [...this.state.todos.slice(0, index),newTodo, ...this.state.todos.slice(index + 1)]
+        this.setState(st => ({
+            todos: newTodos
+        }));
+    }
+    handleEditTodo(todoKey, editedTodo){  
+        let index = this.state.todos.findIndex(element => element.key == todoKey)
+        let newTodo = {
+            todo: editedTodo,
+            linethrough: this.state.todos[index].linethrough,
+            key: this.state.todos[index].key,
+            toedit: false
         }
         let newTodos = [...this.state.todos.slice(0, index),newTodo, ...this.state.todos.slice(index + 1)]
         this.setState(st => ({
@@ -42,7 +71,16 @@ class TodoList extends Component {
         }));
     }
     render() {
-        let todos = this.state.todos.map(todoElement => <Todo linethrough = {todoElement.linethrough} text={todoElement.todo} key={todoElement.key} deleteTodo={this.handleDeleteTodo} linethroughTodo={this.handleLinethroughTodo}/>)
+        let todos = this.state.todos.map(todoElement => <Todo 
+            linethrough = {todoElement.linethrough} 
+            text={todoElement.todo} 
+            key={todoElement.key} 
+            toedit={todoElement.toedit}
+            deleteTodo={this.handleDeleteTodo} 
+            linethroughTodo={this.handleLinethroughTodo}
+            toeditTodo={this.handleToEditTodo}
+            editTodo={this.handleEditTodo}
+            />)
         return (
             <div>
                 <NewTodoForm addTodo={this.handleAddTodo} />
